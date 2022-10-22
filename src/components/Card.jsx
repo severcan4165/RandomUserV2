6785c7eb5492ee07344d8d8eb152db34721481f9
salih <salihsevercann@gmail.com>
2022-10-22 19:09:43 +0300

    add

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from "react";
 import cardStyle from "./Card.module.scss";
 import axios from "axios";
+import growingMan from "../assets/growing-up-man.svg";
+import growingWoman from "../assets/growing-up-woman.svg";
+import mail from "../assets/mail.svg";
+import man from "../assets/man.svg";
+import map from "../assets/map.svg";
+import padlock from "../assets/padlock.svg";
+import phone from "../assets/phone.svg";
+import woman from "../assets/woman.svg";
+
 const url = "https://randomuser.me/api/";
 const Card = () => {
   const [personInfo, setPersonInfo] = useState({});
@@ -9,7 +18,7 @@ const Card = () => {
     text1: "",
     text2: "",
   });
- 
+
   const apiFetcher = async () => {
     setLoading(true);
     try {
@@ -25,24 +34,22 @@ const Card = () => {
         location: { country },
         login: { password },
       } = person;
-      const fullname = `${first} ${last}`;
+      const name = `${first} ${last}`;
       const personData = {
         email,
         gender,
-        fullname,
+        name,
         image,
         age,
         country,
         phone,
-        password
+        password,
       };
       setPersonInfo(personData);
       setHoverData({
-        text1:"My name is",
-        text2: personData.fullname
+        text1: "My name is",
+        text2: personData.fullname,
       });
-      
-
     } catch (error) {
       console.log(error);
     }
@@ -52,14 +59,70 @@ const Card = () => {
   useEffect(() => {
     apiFetcher();
   }, []);
-  console.log(personInfo);
+  // console.log(personInfo);
+
+  const hoverHandler = (e) => {
+    const { name } = e.target;
+    setHoverData({ text1: `my ${name} is `, text2: personInfo[name] });
+  };
 
   return (
     <div className={cardStyle.card}>
       <nav className={cardStyle.navbar}></nav>
-      <img className={cardStyle.Image} src={personInfo.image} alt="peopleImage" />
-      <div>{hoverData.text1}</div>
-      <div>{hoverData.text2}</div>
+      <img
+        className={cardStyle.Image}
+        src={personInfo.image}
+        alt="peopleImage"
+      />
+      <div className={cardStyle.text}>
+        <div>{hoverData.text1}</div>
+        <div>{hoverData.text2}</div>
+      </div>
+
+      <div className={cardStyle.hoverImages}>
+        <img
+          className={cardStyle.hoverImage}
+          name="name"
+          onMouseOver={hoverHandler}
+          src={personInfo.gender === "male" ? man : woman}
+          alt="genderimage"
+        />
+        <img
+          className={cardStyle.hoverImage}
+          name="email"
+          onMouseOver={hoverHandler}
+          src={mail}
+          alt="mailimage"
+        />
+        <img
+          className={cardStyle.hoverImage}
+          name="age"
+          onMouseOver={hoverHandler}
+          src={personInfo.gender === "male" ? growingMan : growingWoman}
+          alt="growingimage"
+        />
+        <img
+          className={cardStyle.hoverImage}
+          name="country"
+          onMouseOver={hoverHandler}
+          src={map}
+          alt="location"
+        />
+        <img
+          className={cardStyle.hoverImage}
+          name="phone"
+          onMouseOver={hoverHandler}
+          src={phone}
+          alt="phone"
+        />
+        <img
+          className={cardStyle.hoverImage}
+          name="password"
+          onMouseOver={hoverHandler}
+          src={padlock}
+          alt="password"
+        />
+      </div>
     </div>
   );
 };
